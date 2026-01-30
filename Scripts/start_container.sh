72c2ed5bff9c853a074efac1b9e9b0227deb1341
zeba Begum <zeba.wasay17@gmail.com>
2026-01-31 02:20:56 +0530

    Delete Scripts directory

--- Scripts/start_container.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker stop flask-app || true
-docker rm flask-app || true
-docker pull zebac/flask-app:latest
-docker run -d --name flask-app -p 5000:5000 zebac/flask-app:latest
--- Scripts/stop_container.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Stop Docker container
-docker stop flask-app || true
-docker rm flask-app || true
-
-# Clean old deployment files
-rm -rf /home/ec2-user/flask-app/*
