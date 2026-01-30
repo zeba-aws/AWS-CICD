0224ecde2ac6f84efae165c917999209b1151cb8
zeba Begum <zeba.wasay17@gmail.com>
2026-01-30 19:06:58 +0530

    Create start_container.sh

---
+++ Scripts/start_container.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker stop flask-app || true
+docker rm flask-app || true
+docker pull <dockerhub-username>/flask-app:latest
+docker run -d --name flask-app -p 5000:5000 <dockerhub-username>/flask-app:latest
