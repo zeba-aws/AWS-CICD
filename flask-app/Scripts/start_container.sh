f99e3714aa183ccb654b12327ceac00476c361db
zeba Begum <zeba.wasay17@gmail.com>
2026-01-31 23:06:42 +0530

    Update start_container.sh

--- flask-app/Scripts/start_container.sh
+++ flask-app/Scripts/start_container.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker stop flask-app || true
 docker rm flask-app || true
-docker pull zebac/flask-app:latest
+docker push zebac/flask-app:latest
 docker run -d --name flask-app -p 5000:5000 zebac/flask-app:latest
